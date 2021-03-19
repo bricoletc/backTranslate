1abd0be15d50f324ce6ff19a6372f2f9cc30646d
Brice Letcher <bletcher@ebi.ac.uk>
2021-03-19 16:03:44 +0000

    Adapt to cluster

--- snakerun/submit.sh
+++ snakerun/submit.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bsub.py 10 "backTrans" "snakemake -j 10 --cluster-config cluster_config.json --cluster \"bsub -q {cluster.queue} -n {cluster.nCPUs} -M {cluster.memory} -R {cluster.resources} -J {cluster.name} -o {cluster.output} -e {cluster.error}\""
+bsub.py 10 "backTrans" "snakemake -j --latency-wait 200 --cluster-config cluster_config.json --cluster \"bsub -q {cluster.queue} -n {cluster.nCPUs} -M {cluster.memory} -R {cluster.resources} -J {cluster.name} -o {cluster.output} -e {cluster.error}\""
